a5dfa36ae1ad3815f346825a71a9876095bbd7a4
wanxiangdata <wanxiangdata@qq.com>
2018-01-21 18:32:55 +0800

    0121板垣征四郎

---
+++ O文档/writeExif.py
@@ -0,0 +1,15 @@
+
+
+import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")
+import pyexiv2
+metadata = pyexiv2.ImageMetadata('newton.jpg')
+metadata.read()
+print metadata.exif_keys
+print metadata['Exif.Image.DateTime'].value
+
+import datetime
+metadata['Exif.Image.DateTime'].value = datetime.datetime.today()
+metadata.write()
+
+metadata['Exif.Photo.UserComment'] = u'一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十'
+metadata.write()
